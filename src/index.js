89420c3af206de998fdf6c2eaa061e0cc78d4136
eric-lab-star <cyon256@icloud.com>
2022-10-17 21:02:34 +0900

    draw line

--- src/index.js
+++ src/index.js
@@ -1,40 +1,60 @@
 import {
-  Scene,
-  PerspectiveCamera,
-  WebGLRenderer,
   BoxGeometry,
-  MeshBasicMaterial,
+  BufferGeometry,
+  Line,
+  LineBasicMaterial,
   Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  Vector3,
+  WebGLRenderer,
 } from "three";
 
 const scene = new Scene();
 const camera = new PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+  10,
+  100
 );
-const renderer = new WebGLRenderer();
 
-renderer.setSize(innerWidth, innerHeight);
+const renderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new BoxGeometry(1, 1, 1);
-const material = new MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new Mesh(geometry, material);
-scene.add(cube);
-camera.position.z = 5;
+const boxGeometry = new BoxGeometry(10, 10, 10);
+const meshMaterial = new MeshBasicMaterial({ color: 0x00ff00 });
+const lineMaterial = new LineBasicMaterial({ color: 0xffffff, linewidth: 100 });
 
+const points = [];
+points.push(new Vector3(-20, 0, 0));
+points.push(new Vector3(-10, 10, 0));
+points.push(new Vector3(0, 0, 0));
+points.push(new Vector3(-20, 0, 0));
+
+const cube = new Mesh(boxGeometry, meshMaterial);
+const geometry = new BufferGeometry().setFromPoints(points);
+const line = new Line(geometry, lineMaterial);
+scene.add(line, cube);
+
+camera.position.set(0, 0, 50);
+camera.lookAt(0, 0, 0);
+
+function rotate(object) {
+  object.rotation.x += 0.01;
+  object.rotation.y += 0.01;
+  object.rotation.z += 0.01;
+}
+let x = 1;
+cube.translateX(40);
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.z += 0.01;
-  cube.rotation.x += 0.01;
+  // rotate(line);
+  // rotate(cube);
+
   renderer.render(scene, camera);
 }
 
-if (WebGL.isWebGLAvailable()) {
-  animate();
-} else {
-  const warning = WebGL.getWebGLErrorMessage();
-  document.getElementById("container").appendChild(warning);
-}
+animate();
+console.log("hello", x);
