97bb356e25bdb29c4836dddf32b1fde31d75ebf0
eric-lab-star <cyon256@icloud.com>
2022-10-17 22:51:48 +0900

    rotating cube

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {
   BoxGeometry,
   BufferGeometry,
+  CustomBlending,
   Line,
   LineBasicMaterial,
   Mesh,
@@ -13,10 +14,10 @@ import {
 
 const scene = new Scene();
 const camera = new PerspectiveCamera(
-  75,
+  110,
   window.innerWidth / window.innerHeight,
   10,
-  100
+  440
 );
 
 const renderer = new WebGLRenderer();
@@ -38,18 +39,42 @@ const geometry = new BufferGeometry().setFromPoints(points);
 const line = new Line(geometry, lineMaterial);
 scene.add(line, cube);
 
-camera.position.set(0, 0, 50);
-camera.lookAt(0, 0, 0);
+camera.position.set(0, 50, 40);
+camera.lookAt(-50, 0, 10);
 
 function rotate(object) {
   object.rotation.x += 0.01;
   object.rotation.y += 0.01;
   object.rotation.z += 0.01;
 }
-let x = 1;
-cube.translateX(40);
+
+let dx, dy, dz;
+cube.translateX(-50);
+cube.translateZ(-50);
+
+function moveCube() {
+  if (cube.position.x === 50) {
+    dx = -0.4;
+  }
+  if (cube.position.x === -50) {
+    dx = 0.4;
+  }
+
+  cube.translateOnAxis(new Vector3(dx, dy, dz), 10);
+}
+
+function rotateCube() {
+  cube.rotateY(0.05);
+}
+scene.rotateY(1);
+scene.rotateX(0);
+scene.rotateZ(0);
+const x = 10;
 function animate() {
   requestAnimationFrame(animate);
+  // scene.rotateY(0.01);
+  moveCube();
+  rotateCube();
   // rotate(line);
   // rotate(cube);
 
@@ -57,4 +82,3 @@ function animate() {
 }
 
 animate();
-console.log("hello", x);
